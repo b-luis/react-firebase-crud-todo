eb160d6e37567559da7ff99cb9620329c0f68f4c
b-luis <bscarletluis@gmail.com>
2023-09-06 23:46:47 +0800

    feat: note design, config, read, and delete

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./components/Todo";
+import { db, todosCollection } from "./firebase";
+import {
+  doc,
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl text-center font-bold text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 sm:mt-0 bg-gray-400 text-slate-100`,
+  count: `text-center p-2`,
+};
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  // Create Todo
+  // Read Todo from firebase
+  // ! real time
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = [];
+      querySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArr);
+    });
+    return unsubscribe;
+  }, []);
+
+  // ! not real time
+  // useEffect(() => {
+  //   const getTodos = async () => {
+  //     try {
+  //       const data = await getDocs(todosCollection); // READ THE DATA
+  //       const filteredData = data.docs.map((doc) => ({
+  //         ...doc.data(),
+  //         id: doc.id,
+  //       }));
+  //       setTodos(filteredData); // SET THE TODO
+  //       console.log(filteredData);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getTodos();
+  // }, []);
+
+  // Update Todo in Firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  // Delete Todo
+  const deleteTodo = async (todo) => {
+    console.log(todo.id);
+    const todoRef = doc(db, "todos", todo.id);
+    await deleteDoc(todoRef);
+  };
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
